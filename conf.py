608f43f1171fe3d9578b190e53051b3ae7885804
Jason Callaway <jason@jasoncallaway.com>
2016-11-06 17:51:21 -0500

    moving to pip-installed theme

--- conf.py
+++ conf.py
@@ -20,6 +20,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -121,7 +123,8 @@ todo_include_todos = False
 #
 #html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes",]
+#html_theme_path = ["_themes",]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
