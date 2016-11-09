aa8c6d5b512fdb0e383e40009790394fb478f90e
Jason Callaway <jason@jasoncallaway.com>
2016-11-09 17:59:51 -0500

    working on sctm parser

--- master_sctm_parser.py
+++ master_sctm_parser.py
@@ -1,14 +1,39 @@
+import jinja2
+import openpyxl
+import os
 import sys
 
-with open(sys.argv[1], 'r') as f:
-    master_sctm = f.read()
-f.closed()
+from ntmap import *
 
-for line in master_sctm:
-    (nist_ref_num, control_family, org_ref_num,
-     baseline_low, baseline_moderate, baseline_high,
-     confidentiality_low, confidentiality_moderate,
-     confidentiality_high, integrity_low, integrity_moderate,
-     integrity_high, availability_low, availability_moderate,
-     availability_high, references, redhat_response,
-     requirements, suppemental_guidance) = line.split(',')
+# Get the path of this file
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Load the Jinja2 template
+loader = jinja2.FileSystemLoader(dir_path)
+environment = jinja2.Environment(loader=loader)
+template = environment.get_template('security_control.j2')
+
+# Open the SCTM Excel workbook
+wb = openpyxl.load_workbook(sys.argv[1], read_only=True)
+ws = wb['Matrix']
+
+# Build the column index
+column_index = {}
+index_number = 0
+for cell in ws.rows[0]:
+    column_index[int(index_number)] = cell.value
+    index_number += 1
+
+# Build the sctm dict
+sctm = {}
+for row in ws.rows:
+    r = {}
+    i = 0
+    for cell in row:
+        r[column_index[i]] = cell.value
+        i += 1
+
+    r["Title"] = nt_map[r["NIST Ref #"]]
+    # Generate the SCMT .rst for this row
+    print template.generate(r)
+
